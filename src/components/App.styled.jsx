b201cf52724192fb57f58c60fd60f970a81ebe8e
xanderkiev2022 <301019a@gmail.com>
2023-02-09 19:30:44 +0200

    Initial commit

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 400px;
+  margin: 30px auto 30px;
+  padding: 30px;
+  background-color: var(--primary-background-color);
+  box-shadow: var(--box-shadow);
+`;
+
+export const TitleH1 = styled.h1`
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+export const TitleH2 = styled.h2`
+  text-align: center;
+  margin-bottom: 10px;
+`;
---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
